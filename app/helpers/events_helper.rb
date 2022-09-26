d4a141d00b54de718f7e5a54b94ee49d6bd8fff9
JoshuaPaino <joshua.paino@gmail.com>
2022-09-26 17:22:15 -0500

    Init Repo

---
+++ app/helpers/events_helper.rb
@@ -0,0 +1,14 @@
+module EventsHelper
+    def price(event)
+        if event.free?
+            "Free"
+        else
+            number_to_currency(event.price)
+        end
+    end
+
+    def day_and_time(event)
+        event.starts_at.strftime("%B %d at %I:%M %P")
+    end
+
+end
---
+++ app/models/event.rb
@@ -0,0 +1,6 @@
+class Event < ApplicationRecord
+
+    def free?
+        price.blank? || price.zero?
+    end
+end
